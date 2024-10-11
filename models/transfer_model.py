5992a3813f26f7b804fb3062964408d5ce5aeda4
zavidparvez <93022974+zavidparvez@users.noreply.github.com>
2024-10-11 20:29:39 +1100

    Update transfer_model.py

--- models/transfer_model.py
+++ models/transfer_model.py
@@ -3,8 +3,12 @@ from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.models import Sequential, Model
 
 class TransferLearningModel:
-    def build(self, input_shape=(224, 224, 3), num_output_classes=2):
-        base_model = efn.EfficientNetB7(input_shape=input_shape, include_top=False, weights='imagenet')
+    def __init__(self, input_shape=(224, 224, 3), num_output_classes=2):
+        self.input_shape = input_shape
+        self.num_output_classes = num_output_classes
+    
+    def build(self):
+        base_model = efn.EfficientNetB7(input_shape=self.input_shape, include_top=False, weights='imagenet')
         transfer_layer = base_model.get_layer('top_activation')
         conv_model = Model(inputs=base_model.input, outputs=transfer_layer.output)
 
@@ -12,5 +16,5 @@ class TransferLearningModel:
         model.add(conv_model)
         model.add(Flatten())
         model.add(Dense(1024, activation='relu'))
-        model.add(Dense(num_output_classes, activation='softmax'))
+        model.add(Dense(self.num_output_classes, activation='softmax'))
         return model
